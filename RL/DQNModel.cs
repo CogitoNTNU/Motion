cd7bebadfa8be4a69aef8ffa0af0b3969d84163b
Edvard Klavenes <edvardklavenes@gmail.com>
2025-03-11 18:12:26 +0100

    refactor: DQN-model and imports

--- RL/DQNModel.cs
+++ RL/DQNModel.cs
@@ -1,29 +1,29 @@
 using System;
 using TorchSharp;
 using TorchSharp.Modules;
-using TorchSharp.Tensor;
+using static TorchSharp.torch;
 
 namespace Motion
 {
-    public class DQNModel : torch.nn.Module
+    public class DQNModel : nn.Module 
     {
         private Linear fc1, fc2, fc3;
+
         public DQNModel(int inputSize, int outputSize) : base("DQNModel")
         {
-            fc1 = torch.nn.Linear(inputSize, 128);
-            fc2 = torch.nn.Linear(128, 128);
-            fc3 = torch.nn.Linear(128, outputSize);
+            fc1 = nn.Linear(inputSize, 128);
+            fc2 = nn.Linear(128, 128);
+            fc3 = nn.Linear(128, outputSize);
 
             RegisterComponents();
         }
 
-        public override torch.Tensor forward(torch.Tensor x)
+        public Tensor forward(Tensor x) 
         {
-            x = torch.nn.functional.relu(fc1.forward(x));
-            x = torch.nn.functional.relu(fc2.forward(x));
+            x = nn.functional.relu(fc1.forward(x));
+            x = nn.functional.relu(fc2.forward(x));
             x = fc3.forward(x);
             return x;
         }
     }
-
 }
