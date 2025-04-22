d0e66b4bcff2210ff7ed51e8475de58750eda24d
tobiasfremming <tobias.fremming@gmail.com>
2025-04-22 18:48:06 +0200

    feat: implement Evaluate method for agent performance assessment in GeneticAlgorithm class

--- GA/geneticAlgorithm.cs
+++ GA/geneticAlgorithm.cs
@@ -3,6 +3,14 @@ using System.ComponentModel.DataAnnotations;
 using System.Runtime.CompilerServices;
 using Avalonia.OpenGL.Angle;
 
+using System;
+using System.Threading;
+using NumSharp;
+using SixLabors.ImageSharp;
+using Gym.Environments;
+using Gym.Environments.Envs.Classic;
+using Gym.Rendering.Avalonia;
+
 namespace Motion{
 
     class GeneticAlgorithm {
@@ -11,6 +19,36 @@ namespace Motion{
 
         public GeneticAlgorithm() {
             this.population = InitializePopulation();
+        }
+
+
+        public static float Evaluate(Agent agent, int stepsize){
+            CartPoleEnv cp = new CartPoleEnv(AvaloniaEnvViewer.Factory); // or AvaloniaEnvViewer.Factory
+            bool done = true;
+            float total_rerward = 0;
+            var action = 0.0; 
+            
+            for (int i = 0; i < 100_000; i++)
+            {
+                if (done)
+                {
+                    NDArray observation = cp.Reset();
+                    done = false;
+                }
+                else
+                {
+                    var (observation, reward, _done, information) = cp.Step(action); // switching between left and right
+                    action = agent.ForwardPass(observation)[0]; // switching between left and right
+                    done = _done;
+                    // Do something with the reward and observation.
+                    total_rerward += reward;
+                    
+                }
+
+                //Thread.Sleep(15); // Prevent the loop from finishing instantly.
+            }
+
+            return total_rerward;
         }
         
         public Agent[] InitializePopulation(){
@@ -21,6 +59,51 @@ namespace Motion{
             }
             return population;
         }
+
+
+        public Agent GA(Agent[] population){
+
+            for (int i = 0; i < config.NumGenerations; i++) {
+
+                // evaluate
+                for (int j = 0; j < population.Length; j++)
+                {
+                    Evaluate(population[j], 1);
+                }
+
+                List<List<Agent>> species = Speciation.SpeciateAndFitnessSharing(population, 1.0, 0.8, 0.8, 0.3, 0.5);
+
+                List<Agent> nextGeneration = new List<Agent>();
+                int totalPopulation = population.Length;
+
+                foreach (var specie in species)
+                {
+                    if (specie.Count == 0) continue;
+
+                    // Determine number of offspring for this species
+                    double totalAdjustedFitness = species.Sum(s => s.Sum(a => a.AdjustedFitness));
+                    double speciesFitness = specie.Sum(a => a.AdjustedFitness);
+                    int offspringCount = (int)Math.Round((speciesFitness / totalAdjustedFitness) * totalPopulation);
+
+                    // (Optional) Elitism: keep the best agent
+                    Agent elite = specie.OrderByDescending(a => a.Fitness).First();
+                    nextGeneration.Add(elite);
+                    offspringCount--;
+
+                    while (offspringCount-- > 0)
+                    {
+                        var (parent1, parent2) = Selection.TournamentSelection(specie, 3); // Use your species-level method
+                        Agent child = Crossover.Crossover(parent1, parent2);
+                        Mutate.ApplyMutations(child); // implement this yourself
+                        nextGeneration.Add(child);
+                    }
+                }
+                
+            }
+
+        }
+
+
 
         private bool IsNotEdgeInChromosome(EdgeChromosome edge, EdgeChromosome[] chromosomeEdges){
             foreach (EdgeChromosome chromosomeEdge in chromosomeEdges)
@@ -42,6 +125,18 @@ namespace Motion{
                 }
             }
             return true;
+        }
+
+
+        public void main(){
+
+            // Initialize the population
+            Agent[] population = InitializePopulation();
+            bestAgent =  GA(population);
+
+
+            
+            
         }
 
         
